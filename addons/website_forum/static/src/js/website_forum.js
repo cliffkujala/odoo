5b212372aac5a879a02b508a75956116bc76dcb8
Christophe Matthieu <chm@odoo.com>
2014-10-28 12:58:29 +0100

    fix remove forum image karma

--- addons/website_forum/static/src/js/website_forum.js
+++ addons/website_forum/static/src/js/website_forum.js
@@ -213,31 +213,5 @@ $(document).ready(function () {
                 }
             });
         }
-
-        if ($('textarea.load_editor').length) {
-            $('textarea.load_editor').each(function () {
-                if (this['id']) {
-                    CKEDITOR.replace(this['id']).on('instanceReady', CKEDITORLoadComplete);
-                }
-            });
-        }
     }
 });
-
-
-function IsKarmaValid(eventNumber,minKarma){
-    "use strict";
-    if(parseInt($("#karma").val()) >= minKarma){
-        CKEDITOR.tools.callFunction(eventNumber,this);
-        return false;
-    } else {
-        alert("Sorry you need more than " + minKarma + " Karma.");
-    }
-}
-
-function CKEDITORLoadComplete(){
-    "use strict";
-    $('.cke_button__link').attr('onclick','IsKarmaValid(33,30)');
-    $('.cke_button__unlink').attr('onclick','IsKarmaValid(37,30)');
-    $('.cke_button__image').attr('onclick','IsKarmaValid(41,30)');
-}
