8ba986e161db5ae484726b8b939a25c0e4ba81d5
ssh-odoo <ssh@openerp.com>
2014-12-08 10:48:30 +0530

    [Add,imp]:Remove expense 'sheet' concept
    [imp]:raise warning instead of error
    
    improve code

--- addons/hr_expense/__openerp__.py
+++ addons/hr_expense/__openerp__.py
@@ -58,12 +58,9 @@ This module also uses analytic accounting and is compatible with the invoice on 
         'report/hr_expense_report_view.xml',
         'hr_expense_installer_view.xml',
         'views/report_expense.xml',
+        'expense_web_tip.xml'
     ],
     'demo': ['hr_expense_demo.xml'],
-    'test': [
-        'test/expense_demo.yml',
-        'test/expense_process.yml',
-    ],
     'installable': True,
     'auto_install': False,
     'application': True,
